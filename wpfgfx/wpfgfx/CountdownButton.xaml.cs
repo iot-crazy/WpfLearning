58158393d74889945724f01a0daef57e5b203680
Tony Kenny <t.kenny@fugro.com>
2021-03-11 10:27:15 +0100

    changed after some assistance

--- wpfgfx/wpfgfx/CountdownButton.xaml.cs
+++ wpfgfx/wpfgfx/CountdownButton.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,20 +20,87 @@ namespace wpfgfx
     /// </summary>
     public partial class CountdownButton : UserControl
     {
-        public static readonly DependencyProperty ViewModelProperty =
-                DependencyProperty.Register("ViewModel", typeof(string),
+        public static readonly DependencyProperty MessageProperty =
+             DependencyProperty.Register("Message", typeof(string),
+             typeof(CountdownButton), new FrameworkPropertyMetadata(null));
+
+        public static readonly DependencyProperty FinalMessageProperty =
+             DependencyProperty.Register("FinalMessage", typeof(string),
+             typeof(CountdownButton), new FrameworkPropertyMetadata(null));
+
+        public static readonly DependencyProperty SecondsProperty =
+                     DependencyProperty.Register("Seconds", typeof(TimeSpan),
+                     typeof(CountdownButton), new FrameworkPropertyMetadata(null));
+
+        public static readonly DependencyProperty RemainingSecondsProperty =
+                DependencyProperty.Register("RemainingSeconds", typeof(int),
                 typeof(CountdownButton), new FrameworkPropertyMetadata(null));
+
 
         public CountdownButton()
         {
             InitializeComponent();
         }
 
-        public CountdownButtonViewModel ViewModel
+
+        public TimeSpan Seconds
         {
-            get { return (CountdownButtonViewModel)GetValue(ViewModelProperty); }
-            set { SetValue(ViewModelProperty, value); }
+            get { return (TimeSpan)GetValue(SecondsProperty); }
+            set
+            {
+                SetValue(SecondsProperty, value);
+            }
         }
+
+        public string Message
+        {
+            get
+            {
+                return (string)GetValue(MessageProperty);
+            }
+            set
+            {
+                SetValue(MessageProperty, value);
+            }
+        }
+
+        public string FinalMessage
+        {
+            get
+            {
+                return (string)GetValue(MessageProperty);
+            }
+            set
+            {
+                SetValue(MessageProperty, value);
+            }
+        }
+
+        public int RemainingSeconds
+        {
+            get
+            {
+                return (int)GetValue(RemainingSecondsProperty);
+            }
+            set
+            {
+                SetValue(RemainingSecondsProperty, value);
+            }
+        }
+
+        //public string DisplayMessage
+        //{
+        //    get
+        //    {
+        //        if (Seconds. == 0)
+        //        {
+        //            return FinalMessage;
+        //        }
+        //        return string.Format(Message, RemainingSeconds);
+        //    }
+        //}
+
+
 
     }
 }
--- wpfgfx/wpfgfx/CountdownButtonViewModel.cs
+++ wpfgfx/wpfgfx/CountdownButtonViewModel.cs
@@ -1,100 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
-using System.Text;
-using System.Windows;
-using System.Windows.Media.Animation;
+﻿using System.ComponentModel;
 
 namespace wpfgfx
 {
-    public class CountdownButtonViewModel : DependencyObject, INotifyPropertyChanged
+    public class CountdownButtonViewModel
     {
-        public static readonly DependencyProperty MessageProperty =
-                    DependencyProperty.Register("Message", typeof(string),
-                    typeof(CountdownButtonViewModel), new FrameworkPropertyMetadata(null));
+        public string Message { get; set; }
 
-        public static readonly DependencyProperty SecondsProperty =
-                      DependencyProperty.Register("Seconds", typeof(int),
-                      typeof(CountdownButtonViewModel), new FrameworkPropertyMetadata(null));
+        public string FinalMessage { get; set; }
 
-        public static readonly DependencyProperty FinalMessageProperty =
-                      DependencyProperty.Register("FinalMessage", typeof(string),
-                      typeof(CountdownButtonViewModel), new FrameworkPropertyMetadata(null));
-
-        public static readonly DependencyProperty CurrentSecondsProperty =
-                      DependencyProperty.Register("CurrentSeconds", typeof(int),
-                      typeof(CountdownButtonViewModel), new FrameworkPropertyMetadata(null));
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        public string Message
-        {
-            get { return (string)GetValue(MessageProperty); }
-            set
-            {
-                SetValue(MessageProperty, value);
-                NotifyPropertyChanged(nameof(DisplayMessage));
-            }
-        }
-
-        public string FinalMessage
-        {
-            get { return (string)GetValue(FinalMessageProperty); }
-            set { SetValue(FinalMessageProperty, value); }
-        }
-
-        public int Seconds
-        {
-            get { return (int)GetValue(SecondsProperty); }
-            set
-            {
-                SetValue(SecondsProperty, value);
-                NotifyPropertyChanged(nameof(DisplayMessage));
-            }
-        }
-
-        public Duration Duration
-        {
-            get
-            {
-                return new Duration(new TimeSpan(0,0,Seconds));
-            }
-        }
-
-        public int CurrentSeconds {
-            get { return (int)GetValue(CurrentSecondsProperty); }
-            set
-            {
-                SetValue(CurrentSecondsProperty, value);
-                NotifyPropertyChanged(nameof(DisplayMessage));
-                NotifyPropertyChanged(nameof(IsEnabled));
-            }
-        }
-
-        public string DisplayMessage
-        {
-            get
-            {
-                if (Seconds == 0)
-                {
-                    return FinalMessage;
-                }
-                return string.Format(Message, Seconds);
-            }
-        }
-
-        public bool IsEnabled
-        {
-            get
-            {
-                return Seconds == 0;
-            }
-        }
-
-        protected void NotifyPropertyChanged([CallerMemberName] string name = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-        }
+        public int Seconds { get; set; }
     }
 }
